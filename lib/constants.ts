a2acc7d986c16b1717659ed4b7b300f5a36fbd1c
Ilyes <ilyeesx@gmail.com>
2026-03-01 16:15:16 +0100

    amélioration

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,3 @@
-export const STRIPE_PAYMENT_LINK = "https://buy.stripe.com/VOTRE_LIEN_ICI";
+export const STRIPE_PAYMENT_LINK = "https://buy.stripe.com/aFacN4gLvfAu6Bt4zfes000";
 export const PRODUCT_PRICE = "129,99€";
 export const BRAND = "NEXGEAR";
